469280b4a171ec91dac1c0caa2aa130738de266d
mellon <eason773@sina.com>
2019-08-14 23:20:06 +0800

    cmh

---
+++ js/src/leetcode/array/majority.js
@@ -0,0 +1,19 @@
+/**
+ * 求众数
+ *  给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ */
+
+
+const majorityElement = (array) => {
+
+    // 算法题，尽量减少使用api
+    // return array.sort()[parseInt(array.length / 2)]
+
+    // let counter = 0
+    // for (let i = 0; i < array.length; i++) {
+        
+    // }
+}
+
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]))
+console.log(majorityElement([3,2,3]))
---
+++ js/src/leetcode/math/pow.js
@@ -0,0 +1,40 @@
+/**
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数
+ *  
+
+示例 1:
+输入: 2.00000, 10
+输出: 1024.00000
+
+示例 2:
+输入: 2.10000, 3
+输出: 9.26100
+
+示例 3:
+输入: 2.00000, -2
+输出: 0.25000
+解释: 2-2 = 1/22 = 1/4 = 0.25
+
+ * 
+ */
+
+const pow = (x, n) => {
+    /*
+        // es6
+        return x ** n
+    */
+    if (n == 0) return 1
+    if (n < 0) {
+        n *= -1
+        x = 1 / x
+    }
+    let result = x
+    while (n != 1) {
+        result = result * x
+        n--
+    }
+    return parseFloat(result).toFixed(6)
+}
+console.log(`pow(2, 10) -> ${pow(2, 10)}`)
+console.log(`pow(2.1, 3) -> ${pow(2.1, 3)}`)
+console.log(`pow(2, -2) -> ${pow(2, -2)}`)
