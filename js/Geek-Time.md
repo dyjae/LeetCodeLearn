d89dcfe20b592af2fcaeffa9548a19fa13bf50f2
melonlly <melonlly@outlook.com>
2019-12-25 17:32:09 +0800

    206. 反转链表

--- js/Geek-Time.md
+++ js/Geek-Time.md
@@ -42,4 +42,14 @@
 
 ##### more more more practice ！
 
-### 2019-12-24
+### 数组 & 链表
+##### 206. 反转链表 /geek-time-40/array/reverseList.js
+> https://leetcode-cn.com/problems/reverse-linked-list/
+##### 24. 两两交换链表中的节点
+> https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+##### 141. 环形链表
+> https://leetcode-cn.com/problems/linked-list-cycle/
+##### 142. 环形链表 II
+> https://leetcode-cn.com/problems/linked-list-cycle-ii/
+##### 25. K 个一组翻转链表
+> https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
---
+++ js/src/geek-time-40/array/reverseList.js
@@ -0,0 +1,58 @@
+/*
+
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head, prev) {
+    if (head) {
+        var tmp = head.next
+        head.next = prev
+        prev = head
+        head = tmp
+        reverseList(head, prev)
+    } else {
+        return prev
+    }
+};
+
+
+// 迭代法   O(n)
+var reverseList = function (head) {
+    var cur = head
+    var prev = null
+    while (cur) {
+        var tmp = cur.next
+        cur.next = prev
+        prev = cur
+        cur = tmp
+    }
+    return prev
+};
+
+
+
+// 单向链表节点
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
