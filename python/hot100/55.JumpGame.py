59ccf6d90b49b72149933048240ca0017e71bf94
Jae <dyjae@vip.qq.com>
2020-11-10 09:19:54 +0800

    feat 修改

--- python/hot100/55.JumpGame.py
+++ python/hot100/55.JumpGame.py
@@ -13,6 +13,8 @@ from typing import List
 class JumpGame:
 
     # 前面点能到达的最大距离是否能到当前点
+    # Runtime: 88 ms, faster than 64.25% of Python3 online submissions for Jump Game.
+    # Memory Usage: 15.9 MB, less than 99.99% of Python3 online submissions for Jump Game.
     def canJump4(self, nums: List[int]) -> bool:
         maxVal = 0
         for index, num in enumerate(nums):
@@ -22,6 +24,7 @@ class JumpGame:
         return True
 
     # 从终点往回找
+    # BUG TIME LIMIT
     def canJump3(self, nums: List[int]) -> bool:
         position = len(nums) - 1
         while position != 0:
@@ -33,18 +36,6 @@ class JumpGame:
                     break
             if not isUpdate: return False
         return True
-
-    # 递归, 遍历每个点,找出能到达最远点,当最远点小于当前点表示此点无法到达
-    def canJump2(self, nums: List[int]) -> bool:
-        maxPosition = 0
-        end = 0
-        for index, num in enumerate(nums):
-            # 当前点大于最远点,此点无法到达,无法到结尾
-            if end < index: return False
-            maxPosition = max(maxPosition, num + index)
-            # 更新终点
-            if end == index: end = maxPosition
-        return maxPosition >= len(nums)
 
     # 贪婪算法
     # 每次取最大
@@ -63,6 +54,6 @@ if __name__ == "__main__":
     nums = [2, 3, 1, 1, 4]
     checked = JumpGame()
     print(checked.canJump(nums))
-    print(checked.canJump2(nums))
+    # print(checked.canJump2(nums))
     print(checked.canJump3(nums))
     print(checked.canJump4(nums))
